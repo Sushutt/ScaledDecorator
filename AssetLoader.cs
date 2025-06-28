48f86fc79ce8575bda08b7a2d1941f2403f39e46
Sushutt <andyduha1981@gmail.com>
2025-06-27 21:04:07 -0700

    Add scale parameter and make some optional

--- AssetLoader.cs
+++ AssetLoader.cs
@@ -22,13 +22,24 @@ namespace ScaledDecorator
                 ConfigNode config = file.config;
                 Debug.Log($"Loading ConfigFile {file.url}");
 
+                // These are important so no default values here
                 asset.assetPath = config.GetValue("assetBundle");
                 asset.prefabName = config.GetValue("prefab");
                 asset.planetName = config.GetValue("parent");
-                asset.rotatesWithParent = config.GetValue("rotatesWithParent").ToLower() == "true";
-                asset.rotation = ConfigNode.ParseVector3(config.GetValue("rotation"));
-                asset.rotationSpeed = ConfigNode.ParseVector3(config.GetValue("rotationSpeed"));
-                asset.localPosition = ConfigNode.ParseVector3(config.GetValue("localPosition"));
+
+                asset.rotatesWithParent = config.GetValue("rotatesWithParent") == null || config.GetValue("rotatesWithParent").ToLower() == "true";
+                asset.rotation = config.GetValue("rotation") != null ? 
+                    ConfigNode.ParseVector3(config.GetValue("rotation")) : Vector3.zero;
+
+                asset.rotationSpeed = config.GetValue("rotationSpeed") != null ? 
+                    ConfigNode.ParseVector3(config.GetValue("rotationSpeed")) : Vector3.zero;
+
+                asset.localPosition = config.GetValue("localPosition") != null ? 
+                    ConfigNode.ParseVector3(config.GetValue("localPosition")) : Vector3.zero;
+
+                asset.scale = config.GetValue("scale") != null ? 
+                    ConfigNode.ParseVector3(config.GetValue("scale")) : Vector3.one;
+
 
                 // Tell the asset to instantiate itself
                 asset.CreateInstance();
@@ -48,6 +59,7 @@ namespace ScaledDecorator
         public Vector3 rotation; // Initial rotation
         public Vector3 rotationSpeed; // Degrees per second
         public Vector3 localPosition;
+        public Vector3 scale;
 
         // Create itself
         public void CreateInstance()
@@ -67,6 +79,23 @@ namespace ScaledDecorator
 
             // Miscellaneous nonsense
             thingy.transform.localPosition = localPosition;
+            
+            // Because KSP overrides the root's scale
+            foreach (Transform t in thingy.transform)
+            {
+                Vector3 tScale = t.localScale;
+                tScale.x *= scale.x;
+                tScale.y *= scale.y;
+                tScale.z *= scale.z;
+                t.localScale = tScale;
+
+                Vector3 tPos = t.localPosition;
+                tPos.x *= scale.x;
+                tPos.y *= scale.y;
+                tPos.z *= scale.z;
+                t.localPosition = tPos;
+            }
+
             if (rotatesWithParent)
             {
                 thingy.transform.localRotation = Quaternion.Euler(
